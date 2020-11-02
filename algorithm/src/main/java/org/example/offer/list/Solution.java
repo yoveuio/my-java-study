e88177812c73d9baab428bc084074e206086b3de
aliyer <48895882+yoveuio@users.noreply.github.com>
2020-11-02 21:48:27 +0800

    模拟入栈出栈

--- algorithm/src/main/java/org/example/offer/list/Solution.java
+++ algorithm/src/main/java/org/example/offer/list/Solution.java
@@ -2,6 +2,9 @@ package org.example.offer.list;
 
 import org.example.leetcode.ListNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @ClassName Solution
  * @Description TODO
@@ -10,13 +13,38 @@ import org.example.leetcode.ListNode;
  * @Version 1.0
  */
 public class Solution {
+    /**
+     * 判断栈的压入压出是否满足要求
+     * Stack<Integer> stack = new Stack<>();
+     * int i = 0;
+     * for(int num : pushed) {
+     * stack.push(num); // num 入栈
+     * while(!stack.isEmpty() && stack.peek() == popped[i]) { // 循环判断与出栈
+     * stack.pop();
+     * i++;
+     * }
+     * }
+     * return stack.isEmpty();
+     */
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int i = 0;
+        for (int num : pushed) {
+            stack.push(num); // num 入栈
+            while (!stack.isEmpty() && stack.peek() == popped[i]) { // 循环判断与出栈
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
 
     public ListNode deleteNode(ListNode head, int val) {
         ListNode sentinel, front, tail;
         tail = sentinel = new ListNode(-1);
         sentinel.next = head;
         front = head;
-        while (front!=null) {
+        while (front != null) {
             if (front.val == val) {
                 tail.next = front.next;
                 break;
