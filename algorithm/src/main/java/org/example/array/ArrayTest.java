ba42d2e76b8db6f65bc83a05cd31c589c10ca66c
aliyer <48895882+yoveuio@users.noreply.github.com>
2020-11-04 20:11:43 +0800

    插入区间

--- algorithm/src/main/java/org/example/array/ArrayTest.java
+++ algorithm/src/main/java/org/example/array/ArrayTest.java
@@ -1,5 +1,7 @@
 package org.example.array;
 
+import java.util.Arrays;
+
 /**
  * @ClassName ArrayTest
  * @Description TODO
@@ -12,5 +14,7 @@ public class ArrayTest {
     public static void main(String[] args) {
         Solution solution = new Solution();
         //solution.smallerNumbersThanCurrent(new int[]{5,0,10,0,10,6});
+        int[][] insert = solution.insert(new int[][]{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, new int[]{4, 8});
+        System.out.println(Arrays.deepToString(insert));
     }
 }
--- algorithm/src/main/java/org/example/array/Solution.java
+++ algorithm/src/main/java/org/example/array/Solution.java
@@ -12,6 +12,46 @@ import java.util.*;
 public class Solution {
 
     /**
+     * 插入区间
+     * @param intervals 原来的区间，二维数组intervals[left][right]。每一维有两个数字
+     * @param newInterval 插入的区间
+     */
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        //记录插入区间的左右端点
+        int left = newInterval[0];
+        int right = newInterval[1];
+        boolean placed = false;
+        List<int[]> ansList = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (interval[0] > right) {
+                // 在插入区间的右侧且无交集
+                if (!placed) {
+                    //将区间直接插入,插入区间只插入一次
+                    ansList.add(new int[]{left, right});
+                    placed = true;
+                }
+                //将无交集的区间直接插入
+                ansList.add(interval);
+            } else if (interval[1] < left) {
+                // 在插入区间的左侧且无交集
+                ansList.add(interval);
+            } else {
+                // 与插入区间有交集，计算它们的并集
+                left = Math.min(left, interval[0]);
+                right = Math.max(right, interval[1]);
+            }
+        }
+        if (!placed) {
+            ansList.add(new int[]{left, right});
+        }
+        int[][] ans = new int[ansList.size()][2];
+        for (int i = 0; i < ansList.size(); ++i) {
+            ans[i] = ansList.get(i);
+        }
+        return ans;
+    }
+
+    /**
      * 判断一个山脉数组
      */
     public boolean validMountainArray(int[] A) {
