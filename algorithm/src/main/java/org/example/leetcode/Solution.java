d4505f17a9338fb5403a9843d7e41b733e2926ef
aliyer <48895882+yoveuio@users.noreply.github.com>
2020-11-18 14:49:59 +0800

    web_study

--- algorithm/src/main/java/org/example/leetcode/Solution.java
+++ algorithm/src/main/java/org/example/leetcode/Solution.java
@@ -16,6 +16,33 @@ class Solution {
     }
 
     /**
+     * 给出 R 行 C 列的矩阵，其中的单元格的整数坐标为 (r, c)，满足 0 <= r < R 且 0 <= c < C。
+     * 另外，我们在该矩阵中给出了一个坐标为(r0, c0) 的单元格。
+     * 返回矩阵中的所有单元格的坐标，并按到 (r0, c0) 的距离从最小到最大的顺序排，
+     * 其中，两单元格(r1, c1) 和 (r2, c2) 之间的距离是曼哈顿距离，|r1 - r2| + |c1 - c2|。（你可以按任何满足此条件的顺序返回答案。）
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/matrix-cells-in-distance-order
+     */
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] answer = new int[(R-1) * (C -1)][];
+        int count = 0;
+        for (int i=0; i<R; ++i) {
+            for (int j=0; j<C; ++j) {
+                int[] point = new int[]{i, j};
+                answer[count++] = point;
+            }
+        }
+        Arrays.sort(answer, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return (Math.abs(o1[0] - r0) + Math.abs(o1[1] - c0)) -
+                        (Math.abs(o2[0] - r0) + Math.abs(o2[1] - c0));
+            }
+        });
+        return answer;
+    }
+
+    /**
      * 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小
      * 单调栈+贪心解法
      */
---
+++ my_web_study/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * @ClassName App
+ * @Description 主函数
+ * @Author yoveuio
+ * @Date 2020/11/17 21:02
+ * @Version 1.0
+ */
+
+public class App {
+
+
+    public static void main(String[] args) {
+    }
+}
