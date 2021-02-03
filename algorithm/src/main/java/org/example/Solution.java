42ce2e4cf9cc6e40c94632e9b21eaf22918ff13b
aliyer <48895882+yoveuio@users.noreply.github.com>
2021-02-03 21:37:04 +0800

    刷题

--- algorithm/src/main/java/org/example/Solution.java
+++ algorithm/src/main/java/org/example/Solution.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.ArrayList;
-
 /**
  * @author yoveuio
  * @version 1.0
@@ -9,42 +7,42 @@ import java.util.ArrayList;
  * @description 做题
  * @date 2021/1/9 9:20
  */
-public class Solution {
-
-    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
-        if (k > input.length || k < 0 || input.length == 0) return new ArrayList<>();
-        return getKLeast(input, 0, input.length-1, k - 1);
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.reversePairs(new int[]{7,5,6,4});
     }
 
-    private ArrayList<Integer> getKLeast(int[] input, int l, int r, int k) {
-        int j = partition(input, l, r);
-        if (j == k) {
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int i = 0; i <= j; i++) {
-                list.add(input[i]);
+    int[] buffer;
+    final int KMOD = 1000000007;
+    int answer = 0;
+
+    public int reversePairs(int [] array) {
+        buffer = new int[array.length];
+        dfs(array, 0, array.length - 1);
+        return answer;
+    }
+
+    private void dfs(int[] nums, int l, int r) {
+        if (l >= r) return ;
+        int mid = (r - l >> 1) + l;
+        dfs(nums, l, mid);
+        dfs(nums, mid + 1, r);
+        merge(nums, l, r, mid);
+    }
+
+    private void merge(int[] nums, int l, int r, int mid) {
+        int i = l, j = mid + 1;
+        if (r - l + 1 >= 0) System.arraycopy(nums, l, buffer, l, r - l + 1);
+
+        for (int k = l; k <= r; k++) {
+            if (i > mid) nums[k] = buffer[j++];
+            else if (j > r) nums[k] = buffer[i++];
+            else if (buffer[i] <= buffer[j]) nums[k] = buffer[i++];
+            else {
+                answer += (mid - i + 1) % KMOD;
+                nums[k] = buffer[j++];
             }
-            return list;
         }
-        return j > k ? getKLeast(input, l, j - 1, k) : getKLeast(input, j + 1, r, k);
-    }
-
-    private int partition(int[] nums, int l, int r) {
-        int lo = l, hi = r + 1;
-        int v = nums[lo];
-        while (true) {
-            while (++lo <= r && nums[lo] < v);
-            while (--hi > l && nums[hi] > v);
-            if (lo >= hi) break;
-            swap(nums, lo, hi);
-        }
-        swap(nums, l, hi);
-        return hi;
-    }
-
-    public void swap(int[] nums, int i, int j) {
-        if (nums[i] == nums[j]) return;
-        nums[i] ^= nums[j];
-        nums[j] ^= nums[i];
-        nums[i] ^= nums[j];
     }
 }
