d4574bdc945b2d3a65333fed22ca1de7de00600b
aliyer <48895882+yoveuio@users.noreply.github.com>
2021-02-08 22:13:30 +0800

    刷题

--- algorithm/src/main/java/org/example/Solution.java
+++ algorithm/src/main/java/org/example/Solution.java
@@ -1,6 +1,10 @@
 package org.example;
 
-import org.example.leetcode.ListNode;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author yoveuio
@@ -11,79 +15,19 @@ import org.example.leetcode.ListNode;
  */
 class Solution {
 
-    public int[] multiply(int[] A) {
-        int n = A.length;
-        if (A.length <= 1) return null;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int[] answer = new int[n];
-        left[0] = 1;
-        right[n - 1] = 1;
-        for (int i = 1; i < n; i++) {
-            left[i] = left[i - 1] * A[i - 1];
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * A[i + 1];
-        }
-        for (int i = 0; i < n; i++) {
-            answer[i] = left[i] * right[i];
-        }
-        return answer;
+    Map<Character, Boolean> map = new LinkedHashMap<>();
+
+    public void Insert(char ch)
+    {
+        map.put(ch, map.containsKey(ch));
     }
-
-    public boolean duplicate(int numbers[],int length,int [] duplication) {
-        int n = numbers.length;
-        for (int i = 0; i < n; i++) {
-            while (numbers[i] != i) {
-                int tmp = numbers[numbers[i]];
-                if (tmp == numbers[i]) {
-                    duplication[0] = i;
-                    return true;
-                }
-                numbers[numbers[i]] = numbers[i];
-                numbers[i] = tmp;
-            }
+    //return the first appearence once char in current stringstream
+    public char FirstAppearingOnce()
+    {
+        Set<Map.Entry<Character, Boolean>> entries = map.entrySet();
+        for (Map.Entry<Character, Boolean> entry: entries) {
+            if (!entry.getValue()) return entry.getKey();
         }
-        return false;
-    }
-
-    public int Add(int num1,int num2) {
-        while (num2 != 0) {
-            int c = (num1 & num2) << 1;
-            num1 ^= num2;
-            num2 = c;
-        }
-        return num1;
-    }
-
-    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
-        ListNode point1 = pHead1;
-        ListNode point2 = pHead2;
-
-        while (point1 != point2) {
-            point1 = point1 == null ? pHead2 : point1.next;
-            point2 = point2 == null ? pHead1 : point2.next;
-        }
-        return point1;
-    }
-
-    public int GetNumberOfK(int [] array , int k) {
-        return getMinIndex(array, k + 1) - getMinIndex(array, k);
-    }
-
-    public int getMinIndex(int[] nums, int k) {
-        int l = 0, r = nums.length - 1;
-        int mid ;
-
-        while (l <= r) {
-            mid = (r - l >> 1) + l;
-            if (nums[mid] <= k) {
-                r = mid - 1;
-            }
-            else {
-                l = mid + 1;
-            }
-        }
-        return r;
+        throw new ValueException("error");
     }
 }
