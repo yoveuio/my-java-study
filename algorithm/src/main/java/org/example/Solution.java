e8cfa61b36ea5272e49948ec05d36373ad77fa35
aliyer <48895882+yoveuio@users.noreply.github.com>
2021-02-13 09:16:22 +0800

    锁章节更新

--- algorithm/src/main/java/org/example/Solution.java
+++ algorithm/src/main/java/org/example/Solution.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 /**
  * @author yoveuio
  * @version 1.0
@@ -11,33 +8,4 @@ import java.util.Queue;
  * @date 2021/1/9 9:20
  */
 class Solution {
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] a = new int[]{5,2,3,4,1,6,7,0,8};
-        for (int j : a) {
-            solution.Insert(j);
-            System.out.println(solution.GetMedian());
-        }
-    }
-
-    Queue<Integer> max = new PriorityQueue<>((o1, o2) -> o2 - o1);
-    Queue<Integer> min = new PriorityQueue<>();
-
-    public void Insert(Integer num) {
-        if (max.size() != min.size()) {
-            max.offer(num);
-            min.offer(max.poll());
-        }
-        else {
-            min.offer(num);
-            max.offer(min.poll());
-        }
-    }
-
-    public Double GetMedian() {
-        if (max.size() == 0) return 0.0;
-        if (max.size() == min.size()) return (max.peek() + min.peek()) / 2.0;
-        else return 1.0 * max.peek();
-    }
 }
---
+++ class/src/main/java/org/example/lock/Main.java
@@ -0,0 +1,17 @@
+package org.example.lock;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author yoveuio
+ * @version 1.0
+ * @className Main
+ * @description 锁章节
+ * @date 2021/2/12 21:22
+ */
+public class Main {
+    /**
+     * 包含两个队列：同步队列和条件队列
+     */
+    AbstractQueuedSynchronizer aqs;
+}
